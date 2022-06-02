3ba9e1627921840a965933e8f5f4bba11f3eadb3
mamm1504 <s215805@student.dtu.dk>
2022-06-02 10:14:04 +0200

    Inital setup of Spring Boot Rest API

---
+++ RoborallyAPI/src/main/java/dtu/compute/RoborallyAPI/RoborallyApiApplication.java
@@ -0,0 +1,13 @@
+package dtu.compute.RoborallyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoborallyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoborallyApiApplication.class, args);
+	}
+
+}
---
+++ RoborallyAPI/src/test/java/dtu/compute/RoborallyAPI/RoborallyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.dtu.compute.RoborallyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoborallyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

67551107050fdc0e4516452f55b1b66698149734
mamm1504 <s215805@student.dtu.dk>
2022-06-02 17:09:31 +0200

    Smaller fixes to loading JSON from test file. Need to fix relative path asap!

--- RoborallyAPI/src/main/java/dtu/compute/RoborallyAPI/GameService.java
+++ RoborallyAPI/src/main/java/dtu/compute/RoborallyAPI/GameService.java
@@ -1,7 +1,5 @@
 package dtu.compute.RoborallyAPI;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import dtu.compute.RoborallyAPI.Model.Board;
 import org.springframework.stereotype.Service;
 
@@ -13,16 +11,14 @@ public class GameService implements IGameService {
     private Board board;
     private String gameData;
 
-    Gson gson = new Gson();
     DataFile dataFile = new DataFile("test.json");
 
     public GameService() throws IOException {
-        //loadBoard();
+        loadGame();
     }
 
-    private void loadBoard() throws IOException {
-        String jsonString = dataFile.load();
-        board = gson.fromJson(jsonString, new TypeToken<Board>(){}.getType());
+    private void loadGame() throws IOException {
+        gameData = dataFile.load();
     }
 
     @Override

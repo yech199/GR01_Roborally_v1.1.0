9b83b55c0d0c81165fc37d259b45f941bfbbd451
Silja Sandersen <silja.sandersen@hotmail.com>
2022-06-04 11:47:55 +0200

    Added defaulttile for tiles with walls

---
+++ Model_data/src/main/java/model/boardElements/DefaultTile.java
@@ -0,0 +1,11 @@
+package model.boardElements;
+
+import controller.AGameController;
+import model.Space;
+
+public class DefaultTile extends SpaceElement {
+
+    @Override
+    public void doAction(AGameController gameController, Space space) {
+    }
+}

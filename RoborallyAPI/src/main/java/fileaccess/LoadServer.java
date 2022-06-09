c87f0447251b6820873905223022965527b4b752
mamm1504 <s215805@student.dtu.dk>
2022-06-09 08:38:16 +0200

    Made some serialization on the server

---
+++ RoborallyAPI/src/main/java/fileaccess/LoadServer.java
@@ -0,0 +1,136 @@
+package fileaccess;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import fileaccess.model.BoardTemplate;
+import fileaccess.model.CommandCardFieldTemplate;
+import fileaccess.model.PlayerTemplate;
+import fileaccess.model.SpaceTemplate;
+import model.*;
+import model.boardElements.SpaceElement;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class LoadServer {
+
+    private static void loadSpaces(BoardTemplate template, Board board) {
+        for (SpaceTemplate spaceTemplate : template.spaces) {
+            Space space = board.getSpace(spaceTemplate.x, spaceTemplate.y);
+            if (space != null) {
+                space.getActions().addAll(spaceTemplate.actions);
+                space.getWalls().addAll(spaceTemplate.walls);
+            }
+        }
+    }
+
+    public static void loadPlayers(List<PlayerTemplate> players, Board board) {
+        // Loading players
+        for (PlayerTemplate player : players) {
+            Player newPlayer = new Player(board, player.color, player.name);
+            newPlayer.setSpace(board.getSpace(player.spaceX, player.spaceY));
+            newPlayer.setHeading(Heading.valueOf(player.heading));
+
+            newPlayer.setCards(loadCommandCardFields(player.cards, newPlayer));
+            newPlayer.setRegisters(loadCommandCardFields(player.registers, newPlayer));
+
+            // If phase is INITIALISATION, check for a template player that new player can replace.
+            // Else, check replace the same player with the new data
+            if (board.getPhase() == Phase.INITIALISATION) {
+                newPlayer.activePlayer = player.active;
+                board.getRobot().ifPresent(integer -> board.getPlayers().set(integer, newPlayer));
+            } else {
+                for (int i = 0; i < board.getPlayers().size(); i++) {
+                    if(board.getPlayers().get(i).getName().equals(newPlayer.getName())) {
+                        board.getPlayers().set(i, newPlayer);
+                    }
+                }
+            }
+        }
+    }
+
+    private static CommandCardField[] loadCommandCardFields(ArrayList<CommandCardFieldTemplate> commandCardFields, Player newPlayer) {
+        int i = 0;
+        CommandCardField[] newCards = new CommandCardField[commandCardFields.size()];
+        for (CommandCardFieldTemplate commandCardFieldTemplate : commandCardFields) {
+            CommandCardField commandCardField = new CommandCardField(newPlayer);
+            String command = commandCardFieldTemplate.command;
+
+            // If the card is not empty we create a commandCard
+            if (!command.equals("")) {
+                CommandCard commandCard = new CommandCard(Command.valueOf(command));
+                commandCardField.setCard(commandCard);
+            }
+            newCards[i] = commandCardField;
+            i++;
+        }
+        return newCards;
+    }
+
+    private static BoardTemplate deserialize(String jsonGameState) {
+        // In simple cases, we can create a Gson object with new Gson():
+        GsonBuilder simpleBuilder = new GsonBuilder().
+                registerTypeAdapter(SpaceElement.class, new Adapter<SpaceElement>());
+        Gson gson = simpleBuilder.create();
+
+        return gson.fromJson(jsonGameState, BoardTemplate.class);
+    }
+
+    public static Board loadGame(String jsonState) {
+        BoardTemplate template = deserialize(jsonState);
+
+        // load board state values into board from templates
+        Board board = new Board(template.width, template.height, template.checkPointAmount, template.boardName);
+
+        loadSpaces(template, board);
+        loadPlayers(template.players, board);
+
+        board.setCurrentPlayer(board.getPlayer(template.currentPlayer));
+        board.setPhase(Phase.valueOf(template.phase));
+        board.setStep(template.step);
+        board.setGameId(template.gameId);
+        board.setMaxAmountOfPlayers(template.maxNumberOfPlayers);
+
+        // Count active players in game
+        AtomicInteger i = new AtomicInteger();
+        board.getPlayers().forEach((player) -> {
+            if (player.activePlayer) i.getAndIncrement();
+        });
+        board.amountOfActivePlayers = i.get();
+
+        return board;
+    }
+
+    public static Board createBoard(String boardState, int numOfPlayers) {
+        BoardTemplate template = deserialize(boardState);
+
+        // load board state values into board from templates
+        Board board = new Board(template.width, template.height, template.checkPointAmount, template.boardName);
+
+        loadSpaces(template, board);
+        loadPlayers(template.players, board);
+
+        board.setCurrentPlayer(board.getPlayer(template.currentPlayer));
+        board.setPhase(Phase.valueOf(template.phase));
+        board.setStep(template.step);
+
+        board.setMaxAmountOfPlayers(numOfPlayers);
+        // Overwrite
+        int playerNo = 1;
+        for(int i = 0; i < numOfPlayers; i++) {
+            Player player = new Player(board, template.players.get(i).color, "Player " + playerNo);
+            player.setSpace(board.getSpace(template.players.get(i).spaceX, template.players.get(i).spaceY));
+            player.setHeading(Heading.valueOf(template.players.get(i).heading));
+            board.addPlayer(player);
+            playerNo++;
+        }
+        board.maxAmountOfPlayers = 0;
+        return board;
+    }
+
+    public static Board loadBoard(String boardName, int numOfPlayers) {
+        String json = IOUtil.readGame(boardName, false);
+        return createBoard(json, numOfPlayers);
+    }
+}
---
+++ RoborallyAPI/src/main/java/fileaccess/SaveServer.java
@@ -0,0 +1,119 @@
+package fileaccess;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import fileaccess.model.BoardTemplate;
+import fileaccess.model.CommandCardFieldTemplate;
+import fileaccess.model.PlayerTemplate;
+import fileaccess.model.SpaceTemplate;
+import model.*;
+import model.boardElements.SpaceElement;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SaveServer {
+
+    private static ArrayList<SpaceTemplate> saveSpaces(Board board) {
+        ArrayList<SpaceTemplate> spaceTemplates = new ArrayList<>();
+        // Add all spaces
+        // TODO: Make sure all the needed attributes are saved. e.g. the heading and color of ConveyorBelt
+        for (int i = 0; i < board.width; i++) {
+            for (int j = 0; j < board.height; j++) {
+                Space space = board.getSpace(i, j);
+                if (!space.getWalls().isEmpty() || !space.getActions().isEmpty()) {
+                    SpaceTemplate spaceTemplate = new SpaceTemplate();
+                    spaceTemplate.x = space.x;
+                    spaceTemplate.y = space.y;
+                    spaceTemplate.actions.addAll(space.getActions());
+                    spaceTemplate.walls.addAll(space.getWalls());
+                    spaceTemplates.add(spaceTemplate);
+                }
+            }
+        }
+        return spaceTemplates;
+    }
+
+    private static ArrayList<PlayerTemplate> savePlayers(List<Player> players, Phase phase, Player newPlayer) {
+        ArrayList<PlayerTemplate> playerTemplates = new ArrayList<>();
+        // Save state of player
+        for (Player player : players) {
+            PlayerTemplate playerTemplate = new PlayerTemplate();
+            Space space = player.getSpace();
+            playerTemplate.spaceX = space.x;
+            playerTemplate.spaceY = space.y;
+            playerTemplate.heading = String.valueOf(player.getHeading());
+            playerTemplate.color = player.getColor();
+            playerTemplate.name = player.getName();
+
+            // If Phase is INITIALISATION then we don't save cards. We also only save card if it's the player
+            if (!phase.equals(Phase.INITIALISATION) && newPlayer.equals(player)) {
+                playerTemplate.cards = saveCommandCardFields(player.getCards());
+                playerTemplate.registers = saveCommandCardFields(player.getRegisters());
+            }
+            playerTemplate.active = player.activePlayer;
+            playerTemplates.add(playerTemplate);
+        }
+        return playerTemplates;
+    }
+
+    private static ArrayList<CommandCardFieldTemplate> saveCommandCardFields(CommandCardField[] commandCardFields) {
+        ArrayList<CommandCardFieldTemplate> newCards = new ArrayList<>();
+        for (CommandCardField commandCardField : commandCardFields) {
+            CommandCardFieldTemplate cardFieldTemplate = new CommandCardFieldTemplate();
+
+            if (commandCardField.getCard() == null) {
+                cardFieldTemplate.command = "";
+                cardFieldTemplate.visible = true;
+            } else {
+                cardFieldTemplate.command = String.valueOf(commandCardField.getCard().command);
+                cardFieldTemplate.visible = commandCardField.isVisible();
+            }
+
+            // Add to card template
+            newCards.add(cardFieldTemplate);
+        }
+        return newCards;
+    }
+
+    private static String serialize(BoardTemplate template) {
+        // Saving the board template using GSON
+        GsonBuilder simpleBuilder = new GsonBuilder().
+                registerTypeAdapter(SpaceElement.class, new Adapter<SpaceElement>()).
+                setPrettyPrinting();
+        Gson gson = simpleBuilder.create();
+
+        return gson.toJson(template, template.getClass());
+    }
+
+    public static String serializePlayerState(Board board, Player newPlayer) {
+        // Set up the board template by copying values one by one
+        BoardTemplate template = new BoardTemplate();
+        template.width = board.width;
+        template.height = board.height;
+
+        template.spaces = saveSpaces(board);
+        template.players = savePlayers(board.getPlayers(), board.getPhase(), newPlayer);
+
+        if (board.getCurrentPlayer() == null) template.currentPlayer = 0;
+        else template.currentPlayer = board.getPlayerNumber(board.getCurrentPlayer());
+
+        template.step = board.getStep();
+        template.phase = String.valueOf(board.getPhase());
+        template.boardName = board.getBoardName();
+        template.checkPointAmount = board.totalNoOfCheckpoints;
+        template.maxNumberOfPlayers = board.maxAmountOfPlayers;
+
+        // Count active players in game
+        AtomicInteger i = new AtomicInteger();
+        board.getPlayers().forEach((p) -> {
+            if (p.activePlayer) i.getAndIncrement();
+        });
+        template.activePlayers = i.get();
+
+        if (board.getGameId() != null) template.gameId = board.getGameId();
+
+        return serialize(template);
+    }
+}

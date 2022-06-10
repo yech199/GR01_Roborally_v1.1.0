2050e6d8c9d5cc7815a6d53990a0154101a73652
mamm1504 <s215805@student.dtu.dk>
2022-06-10 22:44:54 +0200

    javadoc

--- RoborallyAPI/src/main/java/dtu/compute/RoborallyAPI/ServerController.java
+++ RoborallyAPI/src/main/java/dtu/compute/RoborallyAPI/ServerController.java
@@ -11,31 +11,61 @@ public class ServerController {
     @Autowired
     private IGameService gameService;
 
-    // Return list of active games on the server
+    /**
+     * Return list of active games on the server
+     * @return JSON string of all games
+     * @author Mads Sørensen
+     */
     @GetMapping("/game")
     public ResponseEntity<String> getListOfGames() {
         return ResponseEntity.ok().body(gameService.getListOfGames());
     }
 
-    // Creates a new game on the server, and returns the state of that game to the client
+    /**
+     * Creates a new game on the server, and returns the state of that game to the client
+     * @param boardname
+     * @param numOfPlayers
+     * @return gameId
+     * @aythor Mads Sørensen
+     */
     @PostMapping("/game/{boardname}/{numOfPlayers}")
     public ResponseEntity<String> createGameFromBoard(@PathVariable String boardname, @PathVariable int numOfPlayers) {
         return ResponseEntity.ok().body(gameService.createGame(boardname, numOfPlayers));
     }
 
-    // Returns the game state of a game
+    /**
+     * Returns the game state of a game
+     * @param id
+     * @param playername
+     * @return Response code
+     * @author Mads Sørensen
+     */
     @GetMapping("/game/{id}/{playername}")
     public ResponseEntity<String> getPlayerState(@PathVariable int id, @PathVariable String playername) {
         return ResponseEntity.ok().body(gameService.getGameById(id, playername));
     }
 
-    // Updates the game state of a game
+    /**
+     * Updates the game state of a game
+     * @param id
+     * @param playername
+     * @param playerData
+     * @return Response code
+     * @author Mads Sørensen
+     */
     @PutMapping("/game/{id}/{playername}/save")
     public ResponseEntity<String> updateGameState(@PathVariable int id, @PathVariable String playername, @RequestBody String playerData) {
         gameService.updateGame(id, playername, playerData);
         return ResponseEntity.ok().body("OK");
     }
 
+    /**
+     * Delete a player from the game
+     * @param id
+     * @param playerName
+     * @return Response code
+     * @author Mads Sørensen
+     */
     @DeleteMapping("/game/{id}/{playerName}")
     public ResponseEntity<String> leaveGame(@PathVariable int id, @PathVariable String playerName) {
         String result = gameService.leaveGame(id, playerName);
@@ -44,7 +74,10 @@ public class ServerController {
         return ResponseEntity.ok().body("ok");
     }
 
-    // Add player to game players
+    /**
+     * Add player to game players
+     * @author Mads Sørensen
+     */
     @PostMapping("/game/join/{id}")
     public ResponseEntity<String> joinGame(@PathVariable int id, @RequestBody String playerName) {
         String result = gameService.joinGame(id, playerName);
@@ -53,19 +86,28 @@ public class ServerController {
         return ResponseEntity.ok().body(result);
     }
 
-    // Return list of board templates
+    /**
+     * Return list of board templates
+     * @author Mark Nielsen
+     */
     @GetMapping("/board")
     public ResponseEntity<String> getListOfBoards() {
         return ResponseEntity.ok().body(gameService.getListOfBoards());
     }
 
-    // Returns a boards configuration/state
+    /**
+     * Return list of board templates
+     * @author Mads Sørensen
+     */
     @GetMapping("/board/{gameId}")
     public ResponseEntity<String> getBoardState(@PathVariable int gameId) {
         return ResponseEntity.ok().body(gameService.getBoardState(gameId));
     }
 
-    // Updates the player state
+    /**
+     * Updates the player state
+     * @author Mads Sørensen
+     */
     @PutMapping("game/{id}/{playername}")
     public ResponseEntity<String> setPlayerCards(@PathVariable int id, @PathVariable String playername, @RequestBody String playerData) {
         String result = gameService.setPlayerState(id, playername, playerData);
